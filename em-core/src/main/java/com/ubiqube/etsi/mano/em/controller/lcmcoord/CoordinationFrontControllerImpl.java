dc0811b40f0f769d72c88c0bd7a313718b483e15
Olivier Vignaud <ovi@ubiqube.com>
2023-07-25 12:31:37 +0200

    Make jadoc possible.

--- em-core/src/main/java/com/ubiqube/etsi/mano/em/controller/lcmcoord/CoordinationFrontControllerImpl.java
+++ em-core/src/main/java/com/ubiqube/etsi/mano/em/controller/lcmcoord/CoordinationFrontControllerImpl.java
@@ -12,7 +12,7 @@
  *     GNU General Public License for more details.
  *
  *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *     along with this program.  If not, see https://www.gnu.org/licenses/.
  */
 package com.ubiqube.etsi.mano.em.controller.lcmcoord;
 
@@ -23,7 +23,7 @@ import com.ubiqube.etsi.mano.vnfm.fc.lcmcoord.CoordinationFrontController;
 
 /**
  *
- * @author Olivier Vignaud <ovi@ubiqube.com>
+ * @author Olivier Vignaud {@literal <ovi@ubiqube.com>}
  *
  */
 @Service
